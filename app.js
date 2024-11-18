c888551c94d10e83ea7d170bf5bdbde417c2eb4b
Priyanshsharma21 <piyuindi220@gmail.com>
2024-11-18 10:51:17 +0530

    cors

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 // cors middleware
-app.use(cors());
+app.use(cors({
+    origin: 'https://drf-tool.vercel.app',
+}))
 
 // logging middleware
 app.use(morgan("tiny"));
